97927e613033c4b6851301b481b553bfd5d48908
lucas501 <lucas.lelann@gmail.com>
2022-07-05 13:57:34 +0000

    add tim test

--- main.R
+++ main.R
@@ -2,18 +2,36 @@ library(tercen)
 library(dplyr)
 library(reshape2)
 
-data = (ctx = tercenCtx())  %>% 
+#library(tim)
+#options("tercen.workflowId" = "6015a4dd34cef273755e1a1b1500427b")
+#options("tercen.stepId"     = "d31241f6-173f-473a-9307-2b4b3c5c0882")
+
+ctx <- tercenCtx()
+
+data <- ctx %>% 
   select(.ci, .ri, .y) %>% 
   reshape2::acast(.ci ~ .ri, value.var='.y', fill=NaN, fun.aggregate=mean) 
 
-colnames(data) = paste('c', colnames(data), sep='')
+colnames(data) <- paste('c', colnames(data), sep='')
 
 seed <- NULL
 if(!ctx$op.value('seed') == "NULL") seed <- as.integer(ctx$op.value('seed'))
 
-dataK = kmeans(data, centers = as.integer(ctx$op.value('centers')), iter.max = as.integer(ctx$op.value('iter.max')), nstart = as.integer(ctx$op.value('nstart')))
+centers <- ctx$op.value("centers",as.integer, "10")
+iter.max <- ctx$op.value("iter.max",as.integer, "10")
+nstart <- ctx$op.value("nstart",as.integer, "1")
 
-data.frame(.ci = seq(from=0,to=length(dataK$cluster)-1),
+#seed<-42
+#centers<-2
+set.seed(seed)
+
+dataK <- kmeans(data, centers = centers , iter.max = iter.max, nstart = nstart)
+
+df.out<-data.frame(.ci = seq(from=0,to=length(dataK$cluster)-1),
            cluster=paste0("cluster", dataK$cluster)) %>%
-  ctx$addNamespace() %>%
+  ctx$addNamespace() 
+
+df.out%>%
   ctx$save()
+
+#tim::build_test_data(res_table = df.out, ctx = ctx, test_name = "test1")
